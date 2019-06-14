b5bbe94d98c30fe1074172b84cb5e9c2f71b7733
Raymond <rpbaguio@gmail.com>
2019-06-14 15:09:09 +0800

    Add and removed some assets

--- application/libraries/Authme.php
+++ application/libraries/Authme.php
@@ -1,11 +1,16 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Authme Authentication Library
+ *
+ * @package Authentication
+ * @category Libraries
+ * @author Gilbert Pellegrom
+ * @link http://dev7studios.com
+ * @version 1.0
+ */
 
-if (!defined('BASEPATH')) {
-    exit('No direct script access allowed');
-}
+class Authme {
 
-class Authme
-{
     private $CI;
 
     public function __construct()
@@ -21,28 +26,49 @@ class Authme
         return $this->CI->session->userdata('logged_in');
     }
 
-    public function signin($password)
+    public function login($email, $password)
     {
-        $user = $this->CI->authme_model->_signin(hash('sha256', $password));
-        if ($user) {
-            unset($user->access_code);
-            $this->CI->session->set_userdata(array(
-                'logged_in' => true,
-                'user' => $user
-            ));
-            return true;
+        $user = $this->CI->authme_model->_get_user_by_email($email);
+        $password = hash('sha512', $password);
+        if($user){
+            if($password){
+                unset($user->password);
+                $this->CI->session->set_userdata(array(
+                    'logged_in' => true,
+                    'user' => $user
+                ));
+                $this->CI->authme_model->_set_user_login($user->id, array('last_login' => date('Y-m-d H:i:s')));
+                return true;
+            }
         }
+
         return false;
     }
 
-    public function signout($redirect = false)
+    public function logout($redirect = false)
     {
         $this->CI->session->sess_destroy();
-        if ($redirect) {
-            $this->CI->load->helper('url');
+        if($redirect){
             redirect($redirect, 'refresh');
         }
     }
+
+    public function signup($email, $password)
+    {
+        $user = $this->CI->authme_model->_get_user_by_email($email);
+        if($user) return false;
+
+        $password = hash('sha512', $password);
+        $this->CI->authme_model->_set_new_user($email, $password);
+        return true;
+    }
+
+    public function reset_password($user_id, $new_password)
+    {
+        $new_password = hash('sha512', $new_password);
+        $this->CI->authme_model->update_user($user_id, array('password' => $new_password));
+    }
+
 }
 
 /* End of file: authme.php */
